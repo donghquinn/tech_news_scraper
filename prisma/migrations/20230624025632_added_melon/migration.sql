3649ee53feee057e5dd4026fdd94c626ddcfb9b4
Kim, Dong H <ehdgus1524@gmail.com>
2023-06-24 11:56:39 +0900

    wip

---
+++ prisma/migrations/20230624025632_added_melon/migration.sql
@@ -0,0 +1,17 @@
+-- AlterTable
+ALTER TABLE "BbcTechNews" ALTER COLUMN "founded" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "Climate" ALTER COLUMN "founded" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "Hackers" ALTER COLUMN "founded" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "Melon" ALTER COLUMN "founded" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "NaverNews" ALTER COLUMN "founded" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "naverKin" ALTER COLUMN "founded" DROP DEFAULT;
