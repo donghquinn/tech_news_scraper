86a6c452028d2cdc2075dc204ff42b44360061d8
Kim Donghyun <kimdonghyun@Daniel-Quinns-Mac-Book.local>
2023-07-07 01:30:16 +0900

    fix

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,6 @@ import { handleFunctions } from 'libraries/handler.lib';
 import cron from 'node-cron';
 import { Logger } from 'utils/logger.util';
 
-cron.schedule("28 1 * * *",  () => {
+cron.schedule("58 23 * * *",  () => {
     handleFunctions().catch(err =>  Logger.error("Handler Error: %o", {err}));
   });
