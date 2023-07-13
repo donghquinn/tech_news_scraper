a466a70dfad0384bb49be37b4eedcb09197e1715
Kim Donghyun <kimdonghyun@Daniel-Quinns-Mac-Book.local>
2023-07-13 22:59:19 +0900

    wip: ml news

--- src/libraries/scrape/machine.lib.ts
+++ src/libraries/scrape/machine.lib.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import * as cheerio from 'cheerio';
+import { ht } from "date-fns/locale";
 import { MachineLearningError } from "errors/machine.error";
 import { PrismaLibrary } from "libraries/common/prisma.lib";
 import { MachineLogger } from "utils/logger.util";
@@ -33,10 +34,45 @@ export const getOriginalLink = async(href: string) => {
 
 export const getTopSourceNews = async(html: cheerio.CheerioAPI) => {
     try {
+        const titleArray: Array<string> = [];
+        const hrefArray: Array<string> = [];
+        const originalArray: Array<string> = [];
+
         html("main.container-xl.px-2.px-md-3")
         .children("div.page-body")
         .children("div.row.d-none.d-md-flex")
-        .children("div.col-md-12 col-lg-4 mt-3 mt-lg-0")
+        .children("div.col-md-12.col-lg-4.mt-3.mt-lg-0")
+        .children("div.row.row-cols-1.g-3")
+        .children("div.col")
+        .children("div.card.h-100")
+        .children("div.card-body.pt-2")
+        .children("div.divide-y").each((index, item) => {
+            const title = html(item)
+            .children("div.row")
+            .children("a.d-block.text-dark")
+            .text()
+            .split('\n')
+            .map((line) => line.trim())
+            .filter((l) => l !== "");
+
+            const link = html(item)
+            .children("div.row")
+            .children("a.d-block.text-dark").prop("href")!
+
+            titleArray.push(...title);
+            hrefArray.push(`https://allainews.com/${link}`);
+        })
+
+        for (let i = 0; i <= hrefArray.length-1 ; i += 1) {
+            const originalLink = await getOriginalLink(hrefArray[i]);
+
+            originalArray.push(originalLink);
+        }
+
+        return {
+            originalArray,
+            titleArray
+        }
     } catch (error) {
         throw new MachineLearningError(
             "Get Top Source Machine Learning news",
@@ -103,20 +139,35 @@ export const getMachineLearningNews = async (prisma: PrismaLibrary, today: momen
 
         const html = cheerio.load(response.data);
 
-        const {latestLink, latestTitle} = await getLatestNews(html);
-        // MachineLogger.info("Response: %o",{response: response.data});
-        // const newsArray:Array<MachineLearningArrayType> = [];
-        
+        const {originalArray:latestLink, titleArray:latestTitle} = await getLatestNews(html);
+        const {originalArray:topLink, titleArray:topTitle} = await getTopSourceNews(html);
+
+        for (let i = 0; i <= latestLink.length - 1; i += 1) {
+            await prisma.machineNews.create({
+                data: {
+                    category: "latest",
+                    title: latestTitle[i],
+                    link: latestLink[i],
+                    founded: new Date(today.format('YYYY-MM-DD HH:mm:ss')),
+                }
+            })
+        }
+
+        MachineLogger.info("Insert Latest News Finished");
 
 
-            // await prisma.machineNews.create({
-            //     data: {
-            //         title: titleArray[i],
-            //         link: originalArray[i],
-            //         founded: new Date(today.format('YYYY-MM-DD HH:mm:ss'))
-            //     }
-            // })
-            MachineLogger.info("Insert Finished");
+        for (let j = 0; j <= topLink.length - 1;  j += 1) {
+            await prisma.machineNews.create({
+                data: {
+                    category: "top",
+                    title: topTitle[j],
+                    link: topLink[j],
+                    founded: new Date(today.format('YYYY-MM-DD HH:mm:ss')),
+                }
+            })
+        }
+
+        MachineLogger.info("Insert Top News Finished");
         
     } catch (error) {
         throw new MachineLearningError(
